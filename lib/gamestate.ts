8e0fd460599f68f0bd30502f8c08ec650f237522
Andrew Peace <apeace@gmail.com>
2015-11-07 16:33:49 -0500

    GameState print() function

--- lib/gamestate.ts
+++ lib/gamestate.ts
@@ -96,4 +96,15 @@ class GameState {
     return new GameState(m);
   }
 
+  print(): string {
+    let m = this._matrix;
+    return [
+      m[0][0] + ' | ' + m[0][1] + ' | ' + m[0][2],
+      '---------',
+      m[1][0] + ' | ' + m[1][1] + ' | ' + m[1][2],
+      '---------',
+      m[2][0] + ' | ' + m[2][1] + ' | ' + m[2][2],
+    ].join('\n');
+  }
+
 }
